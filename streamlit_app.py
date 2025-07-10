e9cea0b67daa501e7a3b6d719ae3a4e948f97e68
dorfy99 <lukas.weirowitz@rwth-aachen.de>
2025-07-10 11:02:15 +0000

    reale eingabe größen

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,29 +3,22 @@ import streamlit as st
 st.title('Daten mit Slidern einlesen')
 
 # Erstellen von 9 individuellen Slidern mit Titeln und ausklappbaren Abschnitten
-with st.expander("Abschnitt 1"):
-    slider1 = st.slider('Titel für Slider 1', min_value=0, max_value=100, value=50)
-    slider2 = st.slider('Titel für Slider 2', min_value=0, max_value=100, value=50)
+st.subheader('Allgemeine Daten')
 
-st.subheader('Zwischenüberschrift')
+Anz_ReAss = st.slider('Anzahl Re-Assemblys je linearem Lebenszyklus', min_value=0, max_value=5, value=2)
 
-with st.expander("Abschnitt 2"):
-    slider3 = st.slider('Titel für Slider 3', min_value=0, max_value=100, value=50)
-    slider4 = st.slider('Titel für Slider 4', min_value=0, max_value=100, value=50)
+with st.expander("Ökologie spezifisch"):
+        FußabdruckErste = st.slider('Fußabdruck der 1. Re-Assembly bezogen auf den Fußabdruck einer Neuproduktion', min_value=0, max_value=100, value=10)
+        FußabdruckSteigung = st.slider('Steigung des Fußabdrucks von einer Re-Assembly zur nächsten', min_value=0, max_value=50, value=10)
+        FußabdruckNutzung = st.number_input('Fußabdruck der Nutzung bezogen auf den Fußabdruck der Neuproduktion', min_value=0, value=0)
 
-with st.expander("Abschnitt 3"):
-    slider5 = st.slider('Titel für Slider 5', min_value=0, max_value=100, value=50)
-    slider6 = st.slider('Titel für Slider 6', min_value=0, max_value=100, value=50)
+with st.expander("Kundennutzen spezifisch"):
+    Innovation = st.slider('Särke des Innovationsrückgangs', min_value=0, max_value=10, value=5)
+    
+with st.expander("Ökonomie spezifisch 3"):
+        KostenErste = st.slider('Kosten der 1. Re-Assembly bezogen auf die Kosten einer Neuproduktion', min_value=0, max_value=100, value=10)
+        KostenSteigung = st.slider('Steigung der Kosten von einer Re-Assembly zur nächsten', min_value=0, max_value=50, value=10)
+        Subskription = st.number_input('Höhe der Subskriptionserlöse in einem linearen Lebenszyklus bezogen auf den Verkaufserlös eine linearen Produkts', min_value=0, value=50)
+       
 
-st.subheader('Weitere Zwischenüberschrift')
 
-with st.expander("Abschnitt 4"):
-    slider7 = st.slider('Titel für Slider 7', min_value=0, max_value=100, value=50)
-    slider8 = st.slider('Titel für Slider 8', min_value=0, max_value=100, value=50)
-    slider9 = st.slider('Titel für Slider 9', min_value=0, max_value=100, value=50)
-
-slider_values = [slider1, slider2, slider3, slider4,
-                 slider5, slider6, slider7, slider8,
-                 slider9]
-
-st.write('Eingelesene Werte:', slider_values)
