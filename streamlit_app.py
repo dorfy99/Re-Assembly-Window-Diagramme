a31fc7fd715e08ff5db4849a15fddcf9bab50f62
dorfy99 <lukas.weirowitz@rwth-aachen.de>
2025-07-11 08:04:59 +0000

    erstes test diagramm

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import plotly.graph_objects as go
 
 st.title('Daten mit Slidern einlesen')
 
 # Erstellen von 9 individuellen Slidern mit Titeln und ausklappbaren Abschnitten
-st.subheader('Allgemeine Daten')
+st.subheader('Einflussgrößen auf die Diagramme')
 
-Anz_ReAss = st.slider('Anzahl Re-Assemblys je linearem Lebenszyklus', min_value=0, max_value=5, value=2)
+Anz_ReAss = st.slider('Anzahl Re-Assemblys je linearem Lebenszyklus', min_value=1, max_value=5, value=2)
 
 with st.expander("Ökologie spezifisch"):
         FußabdruckErste = st.slider('Fußabdruck der 1. Re-Assembly bezogen auf den Fußabdruck einer Neuproduktion', min_value=0, max_value=100, value=10)
@@ -21,4 +22,30 @@ with st.expander("Ökonomie spezifisch 3"):
         Subskription = st.number_input('Höhe der Subskriptionserlöse in einem linearen Lebenszyklus bezogen auf den Verkaufserlös eine linearen Produkts', min_value=0, value=50)
        
 
+# Beispiel-Daten
+x_values = [0, 1, 2 ,3 ,4 ,5]
+y_values_curve1 = [0 ,1 ,2 ,3 ,None ,6] # Hier wird der Sprung dargestellt bei x=5 
+y_values_curve2 = [0.5 ,1.5 ,2.5,None,None,None]
 
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(
+    x=x_values,
+    y=y_values_curve1,
+    mode="lines+markers",
+    name="Kurve 1"
+))
+
+fig.add_trace(go.Scatter(
+    x=x_values,
+    y=y_values_curve2,
+    mode="lines+markers",
+    name="Kurve 2"
+))
+
+fig.update_layout(
+    xaxis_title="X-Achse",
+    yaxis_title="Y-Achse"
+)
+
+st.plotly_chart(fig)
